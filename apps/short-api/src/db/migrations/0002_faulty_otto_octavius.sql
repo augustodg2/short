00bb53f508da0e922505b80edcc6eb0f3836d84f
Augusto Dias <augustodg2@gmail.com>
2026-05-09 20:16:57 -0300

    feat(db): add users table

---
+++ apps/short-api/src/db/migrations/0002_faulty_otto_octavius.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"email" text NOT NULL,
+	"password_hash" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "links" ADD COLUMN "user_id" integer;--> statement-breakpoint
+CREATE INDEX "users_email_idx" ON "users" USING btree ("email");--> statement-breakpoint
+ALTER TABLE "links" ADD CONSTRAINT "links_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
--- apps/short-api/src/db/schema.ts
+++ apps/short-api/src/db/schema.ts
@@ -4,6 +4,9 @@ export const links = pgTable(
   "links",
   {
     id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+    userId: integer("user_id").references(() => users.id, {
+      onDelete: "set null",
+    }),
     slug: text("slug").notNull().unique(),
     url: text("url").notNull(),
     expiresAt: timestamp("expires_at"),
@@ -28,4 +31,15 @@ export const clicks = pgTable(
     index("clicks_link_id_idx").on(clicksTable.linkId),
     index("clicks_created_at_idx").on(clicksTable.createdAt),
   ],
+);
+
+export const users = pgTable(
+  "users",
+  {
+    id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+    email: text("email").notNull().unique(),
+    passwordHash: text("password_hash").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (usersTable) => [index("users_email_idx").on(usersTable.email)],
 );
